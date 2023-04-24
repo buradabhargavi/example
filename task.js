c2fdacc45e070388a18a5ab7a28cfe60051d941a
buradabhargavi <110055258+buradabhargavi@users.noreply.github.com>
2023-04-24 13:07:13 +0530

    task13

--- task.js
+++ task.js
@@ -1,17 +1,13 @@
-//create new li element
+function addItem(e) {
+  e.preventDefault();
 
-  var data = document.createElement('li');
 
-  // Add class
-
-  data.className = 'list-group-item'
-
-  //add text node with input value
-
-  data.appendChild(document.createTextNode(newName));
-  data.appendChild(document.createTextNode('  ' + newEmail));
-  //append child
-  form.appendChild(data);
+  //get input value
+  var newName = document.getElementById('name').value;
+  var newEmail = document.getElementById('email').value;
+@@ -15,7 +16,18 @@ function addItem(e) {
+  // localStorage.setItem("name", newName);
+  // localStorage.setItem("email", newEmail);
 
   let infoObj = {
     name: newName,
@@ -20,7 +16,39 @@
 
   let infoObj_serialized = JSON.stringify(infoObj);
 
-  localStorage.setItem("key", infoObj_serialized);
   localStorage.setItem(infoObj.email, infoObj_serialized);
 
   let infoObj_deserialized = JSON.parse(localStorage.getItem("infoObj"))
+  // Add event for delete 
+  //form.addEventListener('click', removeItem);
+  //create new li element
+
+  var data = document.createElement('li');
+@@ -24,22 +36,29 @@ function addItem(e) {
+
+  data.className = 'list-group-item'
+
+  //Create a delete button 
+  var deleteBtn = document.createElement('input');
+  deleteBtn.type = 'button';
+  deleteBtn.value = 'delete';
+
+  deleteBtn.onclick = () => {
+    localStorage.removeItem(infoObj.email);
+    form.removeChild(data);
+  };
+
+  //add classes to delete button 
+  deleteBtn.className = 'btn-delete';
+
+  //Append Text node to delete 
+  deleteBtn.appendChild(document.createTextNode('X'));
+  //add text node with input value
+
+  data.appendChild(document.createTextNode(newName));
+  data.appendChild(document.createTextNode('  ' + newEmail));
+
+  //append child
+  data.appendChild(deleteBtn);
+  form.appendChild(data);
+}
