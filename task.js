2f9bd151b13e4d4cb5df7dda4025a739176bdbe9
buradabhargavi <110055258+buradabhargavi@users.noreply.github.com>
2023-04-24 12:31:01 +0530

    task11commit

--- task.js
+++ task.js
@@ -1,16 +1,26 @@
-var form = document.getElementById('my-form');
-
-// form submit event
-form.addEventListener('submit',addItem);
-
-function addItem(e) {
-  e.preventDefault();
+e.preventDefault();
 
   //get input value
   var newItem = document.getElementById('name').value;
   var newItem1 = document.getElementById('email').value;
+  var newName = document.getElementById('name').value;
+  var newEmail = document.getElementById('email').value;
 
   //put in local storage
   localStorage.setItem("name", newItem);
   localStorage.setItem("email", newItem1);
+  // localStorage.setItem("name", newName);
+  // localStorage.setItem("email", newEmail);
+
+  let infoObj = {
+    name: newName,
+    email: newEmail
+  };
+
+  let infoObj_serialized = JSON.stringify(infoObj);
+
+  localStorage.setItem("key", infoObj_serialized);
+
+  let infoObj_deserialized = JSON.parse(localStorage.getItem("infoObj"))
+
 }
